07d3a29a142373b06a2ed12c1f1f97fbf5c79e56
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2022-09-18 15:54:20 +0300

    Initial Commit

---
+++ TwitterUI/TwitterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUIApp.swift
+//  TwitterUI
+//
+//  Created by Stanislav Lemeshaev on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
